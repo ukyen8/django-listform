996aaad9a3dfb9178da5b660c196df3943fe8cc1
lester <lester_wu@raytrex.com>
2019-05-26 22:47:23 +0800

    redirect after post, and show all items in template

--- lists/tests.py
+++ lists/tests.py
@@ -13,23 +13,23 @@ class HomePageTest(TestCase):
         found = resolve('/')
         self.assertEqual(found.func, home_page)
 
-    # def test_home_page_returns_correct_html(self):
-    #     request = HttpRequest()
-    #     response = home_page(request)
-    #     expected_html = render_to_string('home.html')
-    #     self.assertEqual(response.content.decode(), expected_html)
+    def test_can_save_a_POST_request(self):
+        request = HttpRequest()
+        request.method = "POST"
+        request.POST["item_text"] = 'A new list item'
 
-    # def test_can_save_a_POST_request(self):
-    #     request = HttpRequest()
-    #     request.method = "POST"
-    #     request.POST["item_text"] = 'A new list item'
-    #
-    #     response = home_page(request)
-    #
-    #     self.assertIn("A new list item", response.content.decode())
-    #     expected_html = render_to_string('home.html',
-    #                                      {'new_item_text': 'A new list item'})
-    #     self.assertEqual(response.content.decode(), expected_html)
+        response = home_page(request)
+
+        self.assertEqual(Item.objects.count(), 1)
+        new_item = Item.objects.first()
+        self.assertEqual(new_item.text, 'A new list item')
+
+        self.assertEqual(response.status_code, 302)
+        self.assertEqual(response['location'], '/')
+
+    def test_only_saves_items_when_necessary(self):
+        self.client.get('/')
+        self.assertEqual(Item.objects.count(), 0)
 
 
 class ItemModelTest(TestCase):
