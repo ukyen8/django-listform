bcefe3bf1d6ef21922f95430f3bdd4694ed85a79
lester <lester_wu@raytrex.com>
2019-05-28 15:56:45 +0800

    first steps of FT for layoput + styling

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -112,6 +112,31 @@ class NewVisitorTest(LiveServerTestCase):
 
         # Satisfied, they both go back to sleep
 
+    def test_layout_and_styling(self):
+        # edith go to home page
+        self.browser.get(self.live_server_url)
+        self.browser.set_window_size(1024, 768)
+
+        # she found enter box has been put in center
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        self.assertAlmostEqual(
+            inputbox.location['x'] + inputbox.size['width'] / 2,
+            512,
+            delta=5
+        )
+
+        # She starts a new list and sees the input is nicely
+        # centered there too
+        inputbox.send_keys('testing')
+        inputbox.send_keys(Keys.ENTER)
+        self.wait_for_row_in_list_table('1: testing')
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        self.assertAlmostEqual(
+            inputbox.location['x'] + inputbox.size['width'] / 2,
+            512,
+            delta=5
+        )
+
 
 if __name__ == '__main__':
     unittest.main(warnings='ignore')
